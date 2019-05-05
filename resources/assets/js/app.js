074357fa8c49f45f02d9762216975bcbb120e020
Denis Sinyukov <dnsinyukov@gmail.com>
2019-05-05 23:56:33 +0300

    other code not remember

---
+++ resources/assets/js/app.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+
+import commonComponents from './components';
+
+// commonComponents.forEach(component => {
+//     Vue.component(component.name, component);
+// });
+
+// export default new Vue({
+//   el: "#app"
+// });
