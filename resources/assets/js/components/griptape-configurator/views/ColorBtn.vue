1c8d7f59ba93a8d31cdec2e24c3ffaa96b4ee943
Denis Sinyukov <dnsinyukov@gmail.com>
2019-05-15 02:23:08 +0300

    new color btn

---
+++ resources/assets/js/components/griptape-configurator/views/ColorBtn.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="dropdown">
+        <slot name="btn">
+		    <button>How many colors are in your design? </button>
+		</slot>
+        
+        <div class="dropdown-menu" :aria-labelledby="labelledby">
+            <button 
+                v-for="(color, index) in colors"
+                :key="index"
+                class="dropdown-item file-dropdown" 
+                @click="btnColor = color"
+            >
+                {{ color }}
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+	export default {
+		name: 'color-btn',
+		props: {
+			color: {
+				type: String,
+				default: null
+			},
+            labelledby: {
+                type: String,
+                default: 'dropdownMenuButton'
+            }
+		},
+		data() {
+			return {
+				btnColor: this.color,
+                colors: [
+                    "1 color",
+                    "2 color",
+                    "3 color",
+                    "CMYK",
+                ]
+			}
+		},
+		watch: {
+            btnColor(val) {
+                this.$emit('colorChange', val);
+            }
+        },
+	}
+</script>
