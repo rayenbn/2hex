9f42e995f413846fa9c45de422cd9b1befd6f935
Denis Sinyukov <dnsinyukov@gmail.com>
2019-11-15 00:43:33 +0200

    change the submit-alert text

--- resources/views/summary.blade.php
+++ resources/views/summary.blade.php
@@ -233,7 +233,12 @@
 										&& strlen($auth->position) 
 										&& strlen($auth->phone_num)
 									)
-										@if(($grips->sum('quantity') == 0 && $totalOrders > 1170) || ($totalOrders > 1170 && $grips->sum('quantity') > 0))
+										@php 
+											$gripsQuantity = $grips->sum('quantity');
+											$wheelsQuantity = $wheels->sum('quantity');
+										@endphp
+
+										@if(($gripsQuantity == 0 && $totalOrders > 1170) || ($totalOrders > 1170 && $gripsQuantity > 0))
 											<a href="{{ route('orders.submit') }}" class="btn btn-success m-btn m-btn--custom m-btn--icon m-btn--air">
 												<span>
 													<i class="la la-rocket"></i>
@@ -241,11 +246,20 @@
 												</span>
 											</a>
 										@else
+											@php 
+												$message = 'Your order is too small to add custom grip tapes. Please order at least a total of 400 grip tapes or 50 decks to enable grip tapes.';
+											@endphp
+
+											@if($wheelsQuantity < 300 && $wheelsQuantity != 0)
+												@php 
+													$message = 'Your ordered too few wheels to run a custom wheels production. Please order at least 300 sets of wheels.'; 
+												@endphp
+											@endif
 											<a 
-											href="javascript:void(0);" 
-											class="btn btn-secondary m-btn m-btn--custom m-btn--icon"
-											onclick="alert('Your order is too small to add custom grip tapes. Please order at least a total of 400 grip tapes or 50 decks to enable grip tapes.')"
-										>
+												href="javascript:void(0);" 
+												class="btn btn-secondary m-btn m-btn--custom m-btn--icon"
+												onclick="alert('{{$message}}')"
+											>
 											<span>
 												<i class="la la-rocket"></i>
 												<span>SUBMIT</span>
