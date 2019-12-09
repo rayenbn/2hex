622f14173e67ccd49acab9ebf57dc1479df1d0b8
Denis Sinyukov <dnsinyukov@gmail.com>
2019-12-09 10:51:42 +0200

    Revert "“book2”"
    This reverts commit 8e8ad0400d07ba582c758931732c89ea06fe5a78.

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -13,63 +13,53 @@
 
         <div class="m-content">
             <div class="row">
-                <div class="m-portlet">
+                    <div class="m-portlet">
 
-                    <!-- Begin Mailchimp Signup Form -->
-                    <link href="../../../css/classic-10_7.css" rel="stylesheet" type="text/css" />
-                    <div id="mc_embed_signup">
+            <!-- Begin Mailchimp Signup Form -->
+                        <link href="../../../css/classic-10_7.css" rel="stylesheet" type="text/css" />
+            <div id="mc_embed_signup">
 
-                        <form action="https://2hex.us3.list-manage.com/subscribe/post?u=647f3b724d78e4fe975871794&amp;id=bb8f89e732" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                            <div id="mc_embed_signup_scroll" style="max-width: 700px;">
+                <form action="https://2hex.us3.list-manage.com/subscribe/post?u=647f3b724d78e4fe975871794&amp;id=bb8f89e732" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                    <div id="mc_embed_signup_scroll" style="max-width: 700px;">
 
-                                <h2 style="color: #d46671">THE SKATEBOARD COMPANY FOUNDERS BOOK</h2>
-                                <p style="color: #d46671">RECEIVE THE SKATEBOARD COMPANY FOUNDERS BOOK BY EMAIL</p>
+                        <h2 style="color: #d46671">DOWNLOAD THE SKATEBOARD COMPANY FOUNDERS BOOK</h2>
+                        <p style="color: #d46671">BY 2HEX - YOUR GLOBAL SKATEBOARD SUPPLIER </p>
 
-                                <p>
-                                    <img alt="" src="/skateboard-deck-production/2hex-book-preview.jpg" style="width: 50%" />
-                                </p>
-                                <p style="color: #d46671">PRICE: 0.00 USD (limited time offer)<br>
-                                </p>
+                        <p>Subscribe to our irregular emails about skateboard supplies for skateboard company founders
+                            to <b>download the skateboard company founders book</b> right now!
+                        Fill in the form and we will email the E-book to your provided email address. </p>
+<p>
+    <img alt="" src="/skateboard-deck-production/2hex-book-preview.jpg" style="width: 50%" />
+</p>
+                        <br>
 
-                                <br>
-
-                                <div class="mc-field-group">
-                                    <label for="mce-EMAIL">Email Address </label>
-                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    <div class="mc-field-group">
+                                        <label for="mce-EMAIL">Email Address </label>
+                                        <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    </div>
+                                    <div class="mc-field-group">
+                                        <label for="mce-FNAME">First Name </label>
+                                        <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                    </div>
+                                    <div class="mc-field-group">
+                                        <label for="mce-MMERGE2">Do you have a skateboard business? </label>
+                                        <input type="text" value="" name="MMERGE2" class="" id="mce-MMERGE2">
+                                    </div>
+                                    <div id="mce-responses" class="clear">
+                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                        <div class="response" id="mce-success-response" style="display:none"></div>
+                                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_647f3b724d78e4fe975871794_bb8f89e732" tabindex="-1" value=""></div>
+                                    <div class="clear"><input type="submit" value="Subscribe & Download the Book" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                                 </div>
-                                <div class="mc-field-group">
-                                    <label for="mce-FNAME">First Name </label>
-                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
-                                </div>
-                                <div class="mc-field-group">
-                                    <label for="mce-MMERGE2">Do you have a skateboard business? </label>
-                                    <input type="text" value="" name="MMERGE2" class="" id="mce-MMERGE2">
-                                </div>
-                                <div id="mce-responses" class="clear">
-                                    <div class="response" id="mce-error-response" style="display:none"></div>
-                                    <div class="response" id="mce-success-response" style="display:none"></div>
-                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_647f3b724d78e4fe975871794_bb8f89e732" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Receive the Book by Email*" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                                <br>
-                                <br><br><br>
+                            </form>
+                        </div>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                        <!--End mc_embed_signup-->
 
-                                <p> <b>*Privacy:</b><br>
-                                    <i>Fill in the form, so that we can email your the skateboard company founders book.
-                                        We are curious to know who reads our book, therefor we also ask for your first name and if you already have a skateboard company.
-                                        By submitting the form, you allow us to email you. Apart from the unfinished book we will email you new chapters and information which we believe will help you
-                                        build a successful skateboard company. Of course every email will have an unsubscribe link, but I highly suggest to read a few of our emails first. We email rarely, but when we do it is high quality information for skateboard companies.
-                                        <b>So go ahead and download the skateboard company founders book</b> now!</i></p>
-                            </div>
 
-                        </form>
+
                     </div>
-                    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='MMERGE2';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-                    <!--End mc_embed_signup-->
-
-
-
-                </div>
             </div>
         </div>
     </div>
