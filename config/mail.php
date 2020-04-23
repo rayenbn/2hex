8543939654bf4d6847adfdfac0ce89fbe67b6299
Dev <dev@dev.local>
2020-04-22 22:15:01 -0400

    restore previous one

--- config/mail.php
+++ config/mail.php
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' =>  env('MAIL_USERNAME', 'twohex-com-0001'),
+    'username' =>  env('MAIL_USERNAME', 'goldengolem0815@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD', 'Rt6/8(?)correct'),
+    'password' => env('MAIL_PASSWORD', '123!@#IS SPECIAL'),
 
     /*
     |--------------------------------------------------------------------------
