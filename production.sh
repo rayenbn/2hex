b052eb9bfaa7d99d1856a7ba1245bc018f8c8a33
Denis Sinyukov <dnsinyukov@gmail.com>
2020-05-09 00:12:06 +0300

    Add npm build command

--- production.sh
+++ production.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 php artisan env
+php artisan cache:clear
+php artisan view:clear
 php artisan config:cache
 php artisan route:cache
-php artisan view:clear
 sudo chmod 777 -R ./storage
+npm run production
 composer dump
